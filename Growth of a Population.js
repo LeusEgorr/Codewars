4f9736d6fb744f30a9aa7b0120eeadf28bf3877f
Egor Leus <E.Leus@icloud.com>
2022-12-08 03:11:32 +0300

    Changes

--- Growth of a Population.js
+++ Growth of a Population.js
@@ -1,8 +1,8 @@
 function nbYear(p0, percent, aug, p) {
-let year=0;
-    while(p0<p){year++;
-        p0=p0+p0*(percent/100)+aug}
-    return year
-}
+    let year=0;
+        while(p0<p){year++;
+          p0=p0+Math.floor(p0*percent/100)+aug}
+        return year
+    }
 
-console.log(nbYear(1500,2,50,5000))
+console.log(nbYear(1000,2,50,1200))
