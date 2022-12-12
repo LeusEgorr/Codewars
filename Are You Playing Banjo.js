ee118903e000557053b0edcb86cb9172a9b3d1f6
Egor Leus <E.Leus@icloud.com>
2022-12-12 16:15:33 +0300

    Solution

---
+++ Are You Playing Banjo.js
@@ -0,0 +1,10 @@
+function areYouPlayingBanjo(name) {
+    name.split()
+    if(name[0]=='r' ||name[0]=='R') {
+      name.toString()
+    return name+' plays banjo'}
+    else{
+      name.toString()
+      return name+' does not play banjo'
+    }
+  }
