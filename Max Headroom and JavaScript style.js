81bf8d2a877bdc4bbb3c644f12da4f70d944dcb8
Egor Leus <E.Leus@icloud.com>
2023-01-19 23:35:58 +0300

    Solution

---
+++ Max Headroom and JavaScript style.js
@@ -0,0 +1,19 @@
+function getMax1()
+{
+  var max = 
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2()
+{ 
+ let max1=
+  {
+   name: 'Max Headroom'
+  }
+  return max1
+}
+console.log(getMax1())
+console.log(getMax2())
